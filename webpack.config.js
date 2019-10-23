cba0f80e66366c8fb5f839a7e21776291a887011
Jimmy <jack9462@gmail.com>
2019-10-23 17:55:05 +0800

    Install css loader dependencies and configure webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
         }
       },
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
         test: /\.(png|jpg|gif|mp4|ogg|svg|woff|woff2|ttf|eot)$/,
         loader: 'file-loader'
       }
