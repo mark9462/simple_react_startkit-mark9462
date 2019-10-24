3fc226a760ae9010e33cb1379501b7e86170a5e0
Jimmy <jack9462@gmail.com>
2019-10-24 10:44:56 +0800

    Add index.jsx and configure webpack.config.js

---
+++ src/containers/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+export default hot(() => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>Hello, I`m ReactMaker.</h2>
+      <p>To get started, edit containers/index.jsx</p>
+    </div>
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
@@ -22,6 +23,17 @@ module.exports = {
         test: /\.(png|jpg|gif|mp4|ogg|svg|woff|woff2|ttf|eot)$/,
         loader: 'file-loader'
       }
+    ]
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components/'),
+      containers: path.resolve(__dirname, 'src/containers/')
+    },
+    extensions: ['.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'src/'),
+      path.resolve(__dirname, 'node_modules/')
     ]
   },
   plugins: [
